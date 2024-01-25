e256dae5754b2ac6dd129254a084c2a49de92955
talesxavier1 <Talesxavier1@gmail.com>
2024-01-25 19:53:23 -0300

    TODO

--- jsonSchemaLib/js/FunctionProps.js
+++ jsonSchemaLib/js/FunctionProps.js
@@ -5,5 +5,6 @@ export class FunctionProps {
     constructor({ functionDefinition, tagName }) {
         this.functionDefinition = functionDefinition;
         this.tagName = tagName;
+        //TODO CRIAR VERIFICAÇÃO DE PARAMETRO. PERMITIR SOMENTE FUNÇOES OU OBJETOS DE FUNÇOES.
     }
 }
