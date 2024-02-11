c3ea9dae8231423f72889ffc683f5d0ec312473f
Talesxavier1 <Talesxavier1@gmail.com>
2024-02-11 20:43:52 -0300

    doc

--- jsonSchemaLib/js/BaseModels.js
+++ jsonSchemaLib/js/BaseModels.js
@@ -1,15 +1,59 @@
 import { GUID } from "./guid.js";
 
-
+/**
+ * Classe base dos nodes.
+ * @class
+ */
 export class BaseNodeModel {
+    /**
+     * Id do node.
+     * @type {string}
+     */
     id;
+
+    /**
+     * Id de referência do node com o seu node pai.
+     * @type {string}
+     */
     id_ref = "";
+
+    /**
+     * Nome do node. Campo que é exibido no threeview.
+     * @type {string}
+     */
     text = "";
+
+    /**
+     * Define se o node pode ser expandido ou não.
+     * @type {boolean}
+     */
     expanded = false;
+
+    /**
+     * Define se o node irá possuir botão de adicionar node filho no seu contextMenu.
+     * @type {boolean}
+     */
     contextAddBtn = false;
+
+    /**
+     * Define se o node irá possuir botão de remover node filho no seu contextMenu.
+     * @type {boolean}
+     */
     contextRemoveBtn = true;
+
+    /**
+     * Definições do node.
+     * @type {BaseNodeValueModel}
+     */
     node_value;
 
+    /**
+     * @constructor
+     * @param {string | null} id_ref Id de referência do node pai.
+     * @description {id} Campo recebe um guid. 
+     * @description {node_value} Campo recebe uma instancia de BaseNodeValueModel.
+     *  
+    */
     constructor(id_ref) {
         this.id = GUID.getGUID();
         this.node_value = new BaseNodeValueModel();
@@ -18,8 +62,26 @@ export class BaseNodeModel {
 }
 
 export class BaseNodeValueModel {
+
+    /**
+     * Nome interno do campo. é usado como key na montagem do jsonSchema.
+     * @requires
+     * @type {string}
+     */
     text_name = '';
+
+    /**
+     * Descrição do campo. Pode ser um nome ou uma frase. campo que é exibido no nodeThree.
+     * @requires
+     * @type {string}
+     */
     text_description = '';
+
+    /**
+     * Tipo do campo.
+     * @type {string}
+     * 
+     */
     select_type = null;
     checkbox_required = false;
     checkbox_nullable = false;
